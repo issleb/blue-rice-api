2de06b24c16a01362438cfd7013729a38140fd44
issleb <issleb@gmail.com>
2017-11-24 22:46:54 -0600

    Strips controller.

---
+++ BlueRice.Api/BlueRice.Api.Lambda/Controllers/StripsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlueRice.Api.Core.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueRice.Api.Lambda.Controllers {
+    [Route("api/[controller]")]
+    public class StripsController : Controller {
+
+        // GET api/strips/1
+        [HttpGet("{id}")]
+        public IActionResult Get(int id) {
+            var logic = new StripLogic();
+            var strip = logic.GetStrip(id);
+
+            return Ok(strip);
+        }
+    }
+}
